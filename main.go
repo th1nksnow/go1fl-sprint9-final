b8f0f2ff541e3296c017633ddee0526493c86f5c
th1nksnow <buffallo654@yandex.ru>
2025-10-06 13:31:32 +0300

    rm "youre_code" comm's

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
 	if size == 0 {
 		return []int{}
 	}
@@ -31,7 +30,6 @@ func generateRandomElements(size int) []int {
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
 	if data == nil {
 		return 0
 	}
@@ -53,7 +51,6 @@ func maximum(data []int) int {
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
 	if data == nil {
 		return 0
 	}
@@ -95,18 +92,15 @@ func maxChunks(data []int) int {
 
 func main() {
 	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
-	// ваш код здесь
 	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// // ваш код здесь
 	start := time.Now()
 	max := maximum(data)
 	elapsed := time.Since(start).Microseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d мкс\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
-	// // ваш код здесь
 	start = time.Now()
 	max = maxChunks(data)
 	elapsed = time.Since(start).Microseconds()
